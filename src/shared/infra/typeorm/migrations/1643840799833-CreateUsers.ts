49797bbd18d339712ac0fd77ddf2e9f4d1b8cdc3
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-02-02 19:27:29 -0300

    chore: creating migration of users

---
+++ src/shared/infra/typeorm/migrations/1643840799833-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1643840799833 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

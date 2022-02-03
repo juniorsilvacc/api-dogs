985347732187f39ed358efbe5b99c164e6990ab7
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-02-03 15:34:10 -0300

    feat: creation of send forgot container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import { IUserTokensRepository } from '@modules/users/domain/repositories/IUserTokensRepository';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import '@modules/users/providers';
 import { container } from 'tsyringe';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );

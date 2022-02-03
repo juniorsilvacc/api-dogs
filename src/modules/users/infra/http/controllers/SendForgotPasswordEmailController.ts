b8003a6c11a82576ec55df2973e4f0d614be0983
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-02-03 15:33:41 -0300

    feat: creation of send forgot controller

---
+++ src/modules/users/infra/http/controllers/SendForgotPasswordEmailController.ts
@@ -0,0 +1,21 @@
+import { SendForgotPasswordEmailService } from '@modules/users/services/SendForgotPasswordEmailService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class SendForgotPasswordEmailController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendForgotPassword = container.resolve(
+      SendForgotPasswordEmailService,
+    );
+
+    await sendForgotPassword.execute({
+      email,
+    });
+
+    return response.status(204).send();
+  }
+}
+
+export { SendForgotPasswordEmailController };

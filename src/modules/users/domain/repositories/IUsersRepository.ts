54e495f72d4a23d5ec52d068e6f3420a66ae9126
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-02-03 01:53:55 -0300

    feat: user repository methods

--- src/modules/users/domain/repositories/IUsersRepository.ts
+++ src/modules/users/domain/repositories/IUsersRepository.ts
@@ -3,8 +3,8 @@ import { IUser } from '../models/IUser';
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<IUser>;
-  findByEmail(email: string): Promise<IUser>;
-  findById(id: string): Promise<IUser>;
+  findByEmail(email: string): Promise<IUser | undefined>;
+  findById(id: string): Promise<IUser | undefined>;
 }
 
 export { IUsersRepository };
---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,39 @@
+import { ICreateUserDTO } from '@modules/users/domain/dtos/ICreateUserDTO';
+import { IUser } from '@modules/users/domain/models/IUser';
+import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import { getRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({ username, email, password }: ICreateUserDTO): Promise<IUser> {
+    const user = this.repository.create({
+      username,
+      email,
+      password,
+    });
+
+    await this.repository.save(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<IUser | undefined> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+
+  async findById(id: string): Promise<IUser | undefined> {
+    const user = await this.repository.findOne(id);
+
+    return user;
+  }
+}
+
+export { UsersRepository };

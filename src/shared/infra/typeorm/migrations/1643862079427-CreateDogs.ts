f678d6378a52c525e72b12452c0b5f30b137470b
juniorsilvaeng <juniorsilvacc@hotmail.com>
2022-02-03 01:21:45 -0300

    feat: creation migration of dogs

---
+++ src/shared/infra/typeorm/migrations/1643862079427-CreateDogs.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateDogs1643862079427 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
